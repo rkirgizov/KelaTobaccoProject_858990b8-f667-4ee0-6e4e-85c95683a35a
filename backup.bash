641c7714672b3b6a6e35a1781f71b60d183012ea
rkirgizov <r.kirgizov@live.com>
2025-04-17 23:28:26 +0300

    Backup at 17 апр 2025 г. 23:28:26

---
+++ backup.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+# backup.bash: pull all BG3 Mod Project files into a single place for source code management
+# by mstephenson6, see guide at https://mod.io/g/baldursgate3/r/git-backups-for-mod-projects
+
+set -e;
+
+# TODO Set MOD_SUBDIR_NAME here, example:
+# MOD_SUBDIR_NAME="CircleOfTheSpores_db7e15fd-b2fc-b159-4bbd-1baab34d8c3a"
+# Look in "C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects" d
+# for names of mods you have already started
+MOD_SUBDIR_NAME="KelaTobaccoProject_858990b8-f667-4ee0-6e4e-85c95683a35a"
+
+# This is the MinGW64 path to a Steam install of the toolkit
+BG3_DATA="/d/SteamLibrary/steamapps/common/Baldurs Gate 3/Data"
+
+# These are set according to "Understanding the Mod Locations", as of Nov 2024, from 
+# https://mod.io/g/baldursgate3/r/getting-started-creating-a-new-mod
+SUBDIR_LIST=(
+	"Projects"
+	"Editor/Mods"
+	"Mods"
+	"Public"
+	"Generated/Public"
+)
+
+if [ -z "$MOD_SUBDIR_NAME" ]; then
+	echo "MOD_SUBDIR_NAME must have a value in $(basename $BASH_SOURCE)";
+	return 1 2>/dev/null;
+	exit 1;
+fi
+
+for subdir in "${SUBDIR_LIST[@]}"; do
+	rm -rf "$subdir/$MOD_SUBDIR_NAME";
+	SRC_ABS_PATH="$BG3_DATA/$subdir/$MOD_SUBDIR_NAME";
+	if [ ! -d "$SRC_ABS_PATH" ]; then
+		continue;
+	fi
+	mkdir -p $subdir;
+	cp -a "$SRC_ABS_PATH" "$subdir";
+done;
+
+
+git remote set-url origin git@github.com:rkirgizov/KelaTobaccoProject_858990b8-f667-4ee0-6e4e-85c95683a35a.git
+git add --all
+git commit -m "Backup at `date`"
+git push
